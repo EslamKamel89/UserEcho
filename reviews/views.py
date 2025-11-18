aa5eda522156ada5098848a829a3752acc68a56c
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-18 06:38:58 +0200

    feat(reviews): add reviews app with basic API endpoint and integrate into project

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, JsonResponse  # type: ignore
+from django.shortcuts import render  # type: ignore
 
 # Create your views here.
+
+def test(request:HttpRequest)->JsonResponse:
+    return JsonResponse({'message':"👋 welcome"})
