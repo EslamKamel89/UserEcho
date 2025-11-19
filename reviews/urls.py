ea507d904e663694d3f2ef03e9d49dafd6463b63
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-19 08:18:52 +0200

    feat(reviews): add form submission and thank-you page with static CSS

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns:list[URLPattern] = [
-    path('' , views.home )
+    path('' , views.home ),
+    path('thank-you' , views.thank_you , name='thank-you')
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpRequest, HttpResponse, JsonResponse  # type: ignore
-from django.shortcuts import render  # type: ignore
+from typing import Optional
+
+from django.http import (HttpRequest, HttpResponse,  # type: ignore
+                         HttpResponseRedirect, JsonResponse)
+from django.shortcuts import render
+from django.urls import reverse  # type: ignore
 
 # Create your views here.
 
 def home(request:HttpRequest)->HttpResponse:
+    if request.method == 'POST':
+        username:Optional[str] = request.POST.get('username' , 'Guest')
+        path = reverse('thank-you')
+        return HttpResponseRedirect(f"{path}?username={username}")
     return render(request , 'reviews/index.html')
+
+def thank_you(request:HttpRequest)->HttpResponse :
+    username:Optional[str] = request.GET.get('username' , 'Guest')
+    return render(request , 'reviews/thank-you.html' , {'username' :username})
