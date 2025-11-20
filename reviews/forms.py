51da0a7713904c2be8e869b3d56fc2d7454f56a3
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-20 07:56:27 +0200

    feat(reviews): enhance ReviewForm with styling and add comment field

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,9 +1,23 @@
 from django import forms
+from django.core.validators import MinLengthValidator
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField(max_length=100 , min_length=5 , label='Name' , error_messages={
+    user_name = forms.CharField(
+        widget=forms.TextInput(
+           attrs= {
+                "class":"bg-gray-50 border rounded-lg text-heading text-sm focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body" ,
+                "placeholder" : "Enter your name"
+            }
+        ) ,
+        max_length=100 , min_length=5 , label='Name' , error_messages={
         "required":"Your name must not be empty" ,
         "max_length" : "Please enter a shorter name!" ,
         "min_length" :"please enter a longer name!"
     })
+    comment = forms.CharField(widget=forms.Textarea(
+         attrs= {
+                "class":"bg-gray-50 border rounded-lg text-heading text-sm focus:ring-brand focus:border-brand block w-full px-2.5 py-2 shadow-xs placeholder:text-body" ,
+                "placeholder" : "Comment"
+            }
+        ) , validators=[MinLengthValidator(10)] , required=False)
