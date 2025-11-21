b5c37caca2081a8e861f27a55ffe642e177363dd
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-21 15:46:53 +0200

    feat(reviews): add Review model with rating validation and initial migration

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,8 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=100 )
+    content = models.TextField()
+    rating = models.IntegerField(validators=[MinValueValidator(1) , MaxValueValidator(5)])
