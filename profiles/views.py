9579bd79f866d0c0191d0ce44272a5fedf6f506b
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-24 07:28:56 +0200

    feat(profiles): add basic file upload handling and ignore image files in Git

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,21 @@
+from django.core.files.uploadedfile import UploadedFile
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
 
+def store_file(uploaded_file:UploadedFile):
+    print(uploaded_file)
+    with open('temp/image.png' , 'wb+') as dest:
+        for chunk in uploaded_file.chunks():
+            dest.write(chunk)
+
 class CreateProfileView(View):
     def get(self,request:HttpRequest):
         return render(request, 'profiles/create-profile.html')
     def post(self , request:HttpRequest) :
-        print({"image":request.FILES.get('image') , "username":request.POST.get('username')})
+        image = request.FILES.get('image')
+        if image is not None:
+            store_file(image)
         return HttpResponseRedirect('/profiles')
 
