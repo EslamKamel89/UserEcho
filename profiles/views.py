88d55f01baee1e6579297dc15ec007548601e007
EslamKamel89 <elsamkamelforex@gmail.com>
2025-11-24 07:11:38 +0200

    feat(profiles): implement basic profile creation with image upload

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 
@@ -7,4 +7,6 @@ class CreateProfileView(View):
     def get(self,request:HttpRequest):
         return render(request, 'profiles/create-profile.html')
     def post(self , request:HttpRequest) :
-        pass
+        print({"image":request.FILES.get('image') , "username":request.POST.get('username')})
+        return HttpResponseRedirect('/profiles')
+
